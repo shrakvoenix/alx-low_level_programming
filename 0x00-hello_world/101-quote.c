36364be5a2bab6a8dfdc372e675ea9dd68e058a6
Shakirullah Abdullateef Olaide <poenix1431@gmail.com>
2023-06-16 19:22:57 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 /**
  * main - This code prints a line to a standard error
  * Return: 1
  **/
 int main(void)
 {
-	write(STDOUT_FILENO. "and that place of art is useful\- . Dora Korpar, 2015-10-19\n", 59);
+	write(STDOUT_FILENO. "and that place of art is useful\" . Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
