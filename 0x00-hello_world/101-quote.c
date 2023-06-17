f64dc2cbdc539cbd93c2c6e3b2e32d5fb2159887
Shakirullah Abdullateef Olaide <poenix1431@gmail.com>
2023-06-16 19:06:44 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - This code prints a line to a standard error
+ * Return: 1
+ */
+int main(void)
+{
+	fput("and that place of art is useful\- . Dora Korpar, 2015-10-19\n, stdout");
+	return (1);
+}
