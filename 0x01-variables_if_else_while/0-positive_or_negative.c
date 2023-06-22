a979aab96e0c0cf0f8929b004a97e7ab8e523a73
Shakirullah Abdullateef Olaide <poenix1431@gmail.com>
2023-06-21 19:50:06 -0700

    Positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,20 +10,18 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-{
-	printf("The number is %d\n", n);
-}
+printf("The number is %d\n", n);
 if (n > 0)
 {
-	printf("%d is positive\n");
+	printf("is positive\n");
 }
 else if (n == 0)
 {
-	printf("%d is zero\n");
+	printf("is zero\n");
 }
 else
 {
-	printf("%d is negative\n");
+	printf("is negative\n");
 }
 return (0);
 }
