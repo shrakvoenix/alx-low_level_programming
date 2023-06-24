ae850b430110e7c8bb4c81c9aefc57d124c5cf23
Shakirullah Abdullateef Olaide <poenix1431@gmail.com>
2023-06-23 17:52:28 -0700

    Debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void positive_or_negative(int i)
+{
+	if (i > 3)
+		printf("%d is positive\n", i);
+
+	else if (i > 3)
+		printf("%d is negative\n", i);
+
+	else
+		printf("%d is zero\n", i);
+}
