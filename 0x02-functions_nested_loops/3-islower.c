38841e48c3eb8a8614195ec1aab75831cb20bedc
Shakirullah Abdullateef olaide <phoenx1431@gmail.com>
2023-07-23 19:31:45 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _islower - Write a function that checks for lowercase character.
  *
@@ -7,13 +8,9 @@
 
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
-	{
-		return 1;
-	}
+	if (c >= 'a' && c <= 'z')
+		return (1);
 	else
-	{
-		return 0;
-	}
-	_putchar('\n');
+		return (0);
 }
+
